8c42dbc537520104eaf2598d34da9c73c680fb75
Casimir Pohjanraito <casimir.pohjanraito@solita.fi>
2015-09-14 17:02:08 +0300

    First version of classy-focus.js

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # classy-focus.js
-Separate styles for browser-focus-outlines, mouse and keyboard.
+
+Ever wanted to have separate CSS-styling for browsers focus-indicator outlines, upon mouse and keyboard input?
+
+    - classy-focus.js does that.
+
+    - Enables accessible styling of the browser focus-rectangle:
+
+        * On mouse or keyboard input, toggles CSS-classes.
+
+        * By default, toggles between *.classy-focus-mouse* and *.classy-focus-keyboard* CSS-classes, on *<body>*.
+
+    - Should work on any browser with javascript support, post-2008.
+
+    - Based on (http://github.com/lindsayevans/outline.js)
---
+++ classy-focus.js
@@ -0,0 +1,56 @@
+/** Classy Outline
+    - Enables accessible styling of the browser focus-rectangle.
+    - Toggles between "co-focus-mouse" and "co-focus-keyboard" classes on <body> (default)
+    - Based on http://github.com/lindsayevans/outline.js
+ **/
+(function(d){
+
+	var target_element = 'body',
+        focus_class_mouse = 'classy-focus-mouse',
+        focus_class_keyboard = 'classy-focus-keyboard',
+
+	    dom_events = 'addEventListener' in d,
+	    add_event_listener = function(type, callback) {
+			// Basic cross-browser event handling
+			if (dom_events) {
+				d.addEventListener(type, callback);
+			} else {
+				d.attachEvent('on' + type, callback);
+			}
+		};
+
+	var setTagClass = function( _tag_str, _cl_str ) {
+        var _tgt = d.getElementsByTagName( _tag_str )[0];
+        var _arr = _tgt.getAttribute( "class") || "";
+            _arr = _arr.split(" ");
+
+        if ( _arr.indexOf( _cl_str ) == -1 )  {
+            _arr.push( _cl_str );
+            _tgt.setAttribute( "class", _arr.join(" ").trim() );
+        } 
+    };
+
+    var removeTagClass = function( _tag_str, _cl_str ) {
+        var _tgt = d.getElementsByTagName( _tag_str )[0],
+            _arr = _tgt.getAttribute("class") || "";
+            _arr = _arr.split(" ");
+
+        var _idx = _arr.indexOf( _cl_str );
+        if ( _idx != -1 )  {
+            _arr.splice( _idx, 1 )
+            _tgt.setAttribute( "class", _arr.join(" ") );
+        }
+    };
+
+	// Using mousedown instead of mouseover, so that previously focused elements don't lose focus ring on mouse move
+	add_event_listener('mousedown', function(){
+		removeTagClass( target_element, focus_class_keyboard );
+		setTagClass( target_element, focus_class_mouse );
+	});
+
+	add_event_listener('keydown', function(){
+		removeTagClass( target_element, focus_class_mouse );
+		setTagClass( target_element, focus_class_keyboard );
+	});
+    // Classy-focus ignores 'touchend'
+})(document);
